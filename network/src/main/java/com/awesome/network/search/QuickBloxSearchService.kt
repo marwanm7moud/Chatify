c6be29ef966ff862a416863464d0c7e7cb8554ec
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-08-26 17:32:31 +0300

    create search service and provite it for di

---
+++ network/src/main/java/com/awesome/network/search/QuickBloxSearchService.kt
@@ -0,0 +1,4 @@
+package com.awesome.network.search
+
+interface QuickBloxSearchService {
+}
---
+++ network/src/main/java/com/awesome/network/search/QuickBloxSearchServiceImpl.kt
@@ -0,0 +1,8 @@
+package com.awesome.network.search
+
+import javax.inject.Inject
+
+class QuickBloxSearchServiceImpl @Inject constructor(
+
+) : QuickBloxSearchService {
+}
