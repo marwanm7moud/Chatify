c9bcbab8701d8a6505511f8d34af1a8c445cbb26
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-09-16 17:26:11 +0300

    make new function to load users without any query

--- network/src/main/java/com/awesome/network/search/QuickBloxSearchService.kt
+++ network/src/main/java/com/awesome/network/search/QuickBloxSearchService.kt
@@ -4,5 +4,6 @@ import com.awesome.repository.response.UserDto
 import kotlinx.coroutines.flow.Flow
 
 interface QuickBloxSearchService {
-    suspend fun searchUserByLoginOrFullName(searchValue:String) : List<UserDto>
+    suspend fun searchUserByFullName(searchValue:String) : List<UserDto>
+    suspend fun loadUsersWithoutQuery() : List<UserDto>
 }
